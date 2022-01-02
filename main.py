0311073ac2a4b4329c5d4bc452841bd894188dda
roger <rramosaliaga@gmail.com>
2022-01-01 23:13:06 -0500

    cambio

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-# This is a sample Python script.
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
 def print_hi(name):
@@ -11,6 +8,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Hola Roger')
+    print_hi('Roger Orlando')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
